55d29e1b4ee3a20844a16a80d7fc04cfb1fb405b
quyixiao <2621048238@qq.com>
2020-02-27 23:23:17 +0800

    我找时间我修改

--- src/com/shengsiyuan/kotlin/demo/HelloKotlin16.kt
+++ src/com/shengsiyuan/kotlin/demo/HelloKotlin16.kt
@@ -10,3 +10,42 @@ class MyChild : MyParent() {
 }
 
 
+class MyChild2(override var name: String) : MyParent() {
+
+}
+// 1.val 是可以override val
+// 2.var 是可以override val
+// 3.val 是不可以override var
+// 本质上,val是相当的get方法，var相当于get与set方法
+open class MyParent3 {
+    open fun method() {
+        println("parent method")
+    }
+
+    open val name: String get() = "parent"
+}
+
+
+class MyChild3 : MyParent3() {
+    override fun method() {
+        super.method()
+        println("child method")
+    }
+
+
+    override val name: String
+        get() = super.name + " and child"
+}
+
+fun main(args: Array<String>) {
+    var myChild = MyChild()
+    println(myChild.name)
+    println("============================")
+
+    var myChild3 = MyChild3()
+    myChild3.method()
+    println(myChild3.name)
+
+
+
+}

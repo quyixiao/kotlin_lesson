f1c7ed393de5ffb4f9115de71bfaac2ec0e8abb0
quyixiao <2621048238@qq.com>
2020-02-27 15:20:07 +0800

    rjuq whnt

---
+++ src/com/shengsiyuan/kotlin/demo/HelloKotlin.kt
@@ -0,0 +1,10 @@
+package com.shengsiyuan.kotlin.demo
+
+fun main(args: Array<String>) {
+    print(sum(5,6))
+}
+
+fun sum(a:Int,b:Int):Int{
+    return a + b
+}
+
--- src/com/shengsiyuan/kotlin/demo/HelloWorldDemo.kt
+++ src/com/shengsiyuan/kotlin/demo/HelloWorldDemo.kt
@@ -2,7 +2,7 @@ package com.shengsiyuan.kotlin.demo
 
 import java.util.function.Consumer
 
-
+// java 中函数或者方法不能脱离类单独存在的，
 fun main(args: Array<String>) {
     val list: List<String> = listOf("hello", "word", "hello word")
     for (str in list) {

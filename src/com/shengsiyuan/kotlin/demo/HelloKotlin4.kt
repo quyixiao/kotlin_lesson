715a69db8a93e9ae95ec69526cdf0f9e93fd2fca
quyixiao <2621048238@qq.com>
2020-02-27 16:02:14 +0800

    提交修改

--- src/com/shengsiyuan/kotlin/demo/HelloKotlin4.kt
+++ src/com/shengsiyuan/kotlin/demo/HelloKotlin4.kt
@@ -2,6 +2,8 @@ package com.shengsiyuan.kotlin.demo
 
 fun main(args: Array<String>) {
     println(convert2Int("ab"))
+    printMultiply("2", "4")
+    printMultiply2("2", "a")
 }
 
 
@@ -16,3 +18,28 @@ fun convert2Int(str: String): Int? { //Int 加一个？表示结果可能为空
     return null
 }
 
+
+fun printMultiply(a: String, b: String) {
+    val a2Int = convert2Int(a)
+    var b2Int = convert2Int(b)
+    if (null != a2Int && null != b2Int) {
+        println(a2Int * b2Int)
+    }
+
+}
+
+
+fun printMultiply2(a: String, b: String) {
+    val a2Int = convert2Int(a)
+    var b2Int = convert2Int(b)
+    //println(a2Int * b2Int) 因为可能是空值，可能是空值
+    if (null == a2Int) {
+        println("params not int")
+    } else if (null == b2Int) {
+        println("params not int")
+    } else {
+        println(a2Int * b2Int)
+    }
+
+
+}

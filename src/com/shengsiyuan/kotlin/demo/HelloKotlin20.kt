ec6d8d3d4471206ec6708c29c25b5103e57e97a3
quyixiao <2621048238@qq.com>
2020-02-28 10:57:47 +0800

    提交修改

--- src/com/shengsiyuan/kotlin/demo/HelloKotlin20.kt
+++ src/com/shengsiyuan/kotlin/demo/HelloKotlin20.kt
@@ -1,5 +1,18 @@
 package com.shengsiyuan.kotlin.demo
 
+/***
+ * var propertyName :propertyType = initValue
+ *  get(){
+ *      return field
+ *  }
+ *  set(value){
+ *      field = value
+ *  }
+ *  只读属性是有两个特点，
+ *      1.用val修饰
+ *      2.只有get方法，没有set方法
+ *  backing field : 支撑字段，
+ */
 class ThePerson(address: String, name: String) {
     val age: Int
         get() = 20
@@ -12,6 +25,22 @@ class ThePerson(address: String, name: String) {
             println("set invoked ")
             field = value
         }
+    //其实也是在学python的魔术方法
+    var name: String = name
+        get() { //提示冗余的实现
+            return field
+        }
+        set(value) {
+            field = value
+        }
+
+    // lombok 用相应的注解来实现
+    // @Getter
+    // @Setter
+    // @Data
+    // class ... 本身就是一个Java包
+    //
+    //
 }
 
 fun main(args: Array<String>) {
@@ -20,6 +49,14 @@ fun main(args: Array<String>) {
     println(person.address)
     person.address = "hangzhou"
     println(person.address)
+
+
+    println("***********************如果没有声明，他的get set 方法将会被隐式的声明出来*****************************")
+    println(person.name)
+    person.name = "lisi"
+    println(person.name)
+
+
 }
 
 

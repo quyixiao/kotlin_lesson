6571bb69bece7cf3643e84ae0314a8848cd70e36
quyixiao <2621048238@qq.com>
2020-02-28 15:09:01 +0800

    如果要一个元参的构造方法

--- src/com/shengsiyuan/kotlin/demo/HelloKotlin26.kt
+++ src/com/shengsiyuan/kotlin/demo/HelloKotlin26.kt
@@ -4,7 +4,7 @@ package com.shengsiyuan.kotlin.demo
 /***
  * 1. 扩展函数所定义在的类实例叫做分发接收者（dispatch receiver）
  * 2. 扩展函数所扩展的那个类的实例叫做扩展接收者（extension receviver）
- * 3. 当以上两个名字出现冲突时，扩展接收都的优先级最高
+ * 3. 当以上两个名字出现冲突时，扩展接收(DD)都的优先级最高
  *
  */
 
@@ -17,3 +17,40 @@ class DD {
 
 
 
+class EE{
+    fun method2(){
+        println("EE method")
+    }
+    fun DD.hello(){
+        println(" ee dd hello")
+        method() // 即使在另个类中
+        method2()
+    }
+
+    fun world(dd:DD){
+        dd.hello()
+    }
+
+    fun DD.output(){
+        println(toString())//调用dd的toString
+        println(this@EE.toString())//调用ee的toString
+    }
+
+    fun test(){
+        var dd = DD()
+        dd.output()
+    }
+}
+
+fun main(args: Array<String>) {
+    var ee = EE()
+    ee.method2()
+
+    println("**********************************")
+    var dd = DD()
+    ee.world(dd)
+
+    println("**********************************")
+    ee.test()
+
+}
---
+++ src/com/shengsiyuan/kotlin/demo2/HelloKotlin1.kt
@@ -0,0 +1,63 @@
+package com.shengsiyuan.kotlin.demo2
+
+//数据类-data-class
+// lombok
+//
+/***
+ * 数据类需要满足的要求
+ * 1.主构造方法至少需要一个参数
+ * 2. 所有的主构造方法参数都需要被标记var或者val
+ * 3. 数据不能是抽象的，open 的，sealed的以及inner的
+ * 对于数据类，编译器会自动生成如下内容
+ *
+ * 1.equals/hashCode对
+ * 2.toString()方法,形式为Person(name=....,age=....,adress=...)
+ * 3.针对属性的componentN方法，并且是按照属性的声明顺序来生成的
+ *
+ *  关于数据类成员的继承的要点
+ *  1.如果数据类中显示定义了equals，hashCode或者是toString方法，或者是在数据类的父类中将这些方法声明成final,
+ *      那么，这些方法就不会再生成，转而使用已有的
+ *  2.如果父类拥有了componentN方法，并且这是open的以及返回的兼容的类型，那么，编译器就会在数据类中生成相应的componentN方法，
+ *      并且重写父类的这些方法，如果父类的这些方法由于不兼容的签名或者被定义为final的，那么编译器就会报错
+ *  3. 在数据类中显式的提供componetN方法以及copy方法实现是不允许的。
+ *
+ *  解构声明
+ *      在主构造方法中有多少个参数，就会依次生成对应的componnent1,component2,component3
+ *      这些方法返回的就是对应字段的值，componentN方法就是用来实现解构声明的
+ *
+ *
+ *
+ *
+ */
+
+data class Person(val name: String, var age: Int, var address: String) {
+    // 说明这个类中已经定义了这个方法了
+    // Conflicting overloads: public final fun copy(name: String, age: Int, address: String): Unit defined in com.shengsiyuan.kotlin.demo2.Person
+    //    fun copy(name:String,age:Int,address: String){
+    //}
+
+
+}
+
+fun main(args: Array<String>) {
+    var person = Person("zhangsan", 20, "beijing")
+    //person.name = "xxxxxxx" Val cannot be reassigned
+    person.age = 40
+    println(person)
+
+    //不要将kotlin和底层的class对象混淆
+    person.age = 50
+    println(person.age)
+
+    // copy 方法，表示你想修改已有对象的哪一个属性
+    var person2 = person.copy(address = "hangzhou", age = 2)//如果一定要提供名字，如果不提供名字，可能不是我们想要的
+    println(person2)
+
+    println("******************************************************")
+
+    var (name, age, address) = person //将整体的分解成一个个变量
+    println(name)
+    println(address)
+
+
+}

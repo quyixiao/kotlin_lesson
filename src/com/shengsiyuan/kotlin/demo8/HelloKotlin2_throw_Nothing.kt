c6071da20648fc6f5e4f5fc3988428039c214be6
quyixiao <2621048238@qq.com>
2020-03-02 15:28:10 +0800

    提交修改

--- src/com/shengsiyuan/kotlin/demo8/HelloKotlin2_throw_Nothing.kt
+++ src/com/shengsiyuan/kotlin/demo8/HelloKotlin2_throw_Nothing.kt
@@ -3,13 +3,10 @@ package com.shengsiyuan.kotlin.demo8
 // throw 在 Kotlin 中是一个表达式
 // 我们可以将 throw 作为Elvis 表达式的一部分
 // throw 表达式的类型是一种特殊的类型：Nothing。类型 ，这种类型
-//
+// 在自己的代码中，可以使用 Nothing 来标记永远不会返回的函数
 fun main(args: Array<String>) {
     var str: String?= "a"
     val str2 = str ?: throw IllegalArgumentException("值不能为空")
     println(str2)
-
-
-
 
 }
